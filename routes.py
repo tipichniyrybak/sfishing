42460985ee7c3d12fca98a8bcc9e778c6698f622
nikifor511 <deadnikifor@gmail.com>
2019-12-02 15:04:43 +0300

    c

--- routes.py
+++ routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, request, url_for
 from __init__ import fl_app
-from forms import LoginForm, RegistrationForm, SendForm
-from db_adaptor import DB
-import globals
+# from forms import LoginForm, RegistrationForm, SendForm
+from db_adapter import DB
 import os
 import datetime
 
@@ -13,21 +12,19 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
-@fl_app.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        rec = DB.query("select login_user('" + login_form.username.data + "', '" + login_form.password.data + "')")
-        if rec[0][0] == 2:
-            flash('No such user!')
-        elif rec[0][0] == 1:
-            flash('Invalid password')
-        else:
-            # flash('Login ok!')
-            rec = DB.query("select id from users where \"Username\" = " + "'" + login_form.username.data +"'")
-            globals.user_id = rec[0][0]
-            print(globals.user_id)
-            return redirect('/resolver_frameset')
-
-
-    return render_template('login.html', title='Sign In', form=login_form)
+# @fl_app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     login_form = LoginForm()
+#     if login_form.validate_on_submit():
+#         rec = DB.query("select login_user('" + login_form.username.data + "', '" + login_form.password.data + "')")
+#         if rec[0][0] == 2:
+#             flash('No such user!')
+#         elif rec[0][0] == 1:
+#             flash('Invalid password')
+#         else:
+#             # flash('Login ok!')
+#             rec = DB.query("select id from users where \"Username\" = " + "'" + login_form.username.data +"'")
+#             globals.user_id = rec[0][0]
+#             print(globals.user_id)
+#             return redirect('/resolver_frameset')
+#     return render_template('login.html', title='Sign In', form=login_form)
