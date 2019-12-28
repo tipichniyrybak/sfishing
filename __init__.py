0643a629457ea0d4bf99f801476ea9772ae1cded
nikifor511 <deadnikifor@gmail.com>
2019-12-28 21:30:04 +0300

    cdd

--- __init__.py
+++ __init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from config import Config
-from flask_bootstrap import Bootstrap
 
 fl_app = Flask(__name__)
 fl_app.config.from_object(Config)
-bootstrap = Bootstrap(fl_app)
 
 
 
